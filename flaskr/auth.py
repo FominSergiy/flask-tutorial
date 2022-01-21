447852504bccf067dfef170cf1ce8b611005906e
Sergiy Fomin <fominssergiy@gmail.com>
2022-01-20 20:20:03 -0500

    added a util function that will help maintain user data across each request

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -68,3 +68,16 @@ def login():
 
     return render_template('auth/login.html')
 
+# This function runs before the view functions
+# Regardless of the url (route)
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?',
+            (user_id)
+        ).fetchone()
